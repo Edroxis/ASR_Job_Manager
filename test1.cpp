dfc6037d30e6c3898dfd0419cfa29aaa2fca57f2
Julien Amoros <ju.amoros@laposte.net>
2017-09-18 10:19:31 +0200

    begin test for pause/unpause processes

--- test1.cpp
+++ test1.cpp
@@ -3,30 +3,23 @@
 #include<stdio.h>
 #include<errno.h>
 #include<signal.h>
+#include <list>
 
 using namespace std;
 
 int main( int argc, char* argv[] ){
-	cout << "hello world" << std::endl;
-	
-	int pid = fork();
+	string mystr = "hello\n";
 
-	if(pid == 0){
-		cout << "Hello! I'm child, my pid is " << getpid() << " and my father's is " << getppid() << std::endl;
-		for(int i=0; i<100000000; i++){
-			//cout << "child said " << i << std::endl;
-		}
-	}
-	else if(pid > 0){
-		sleep(1);
-		cout << "Hello! I'm father, child process is: " << pid << ", prepare to die!" << std::endl;
-		cout << "Father: KILL IT WITH FIRE" << std::endl;
-		kill(pid, SIGKILL);
-		cout << "Father: Finally..." << std::endl;
-	}
-	else{
-		cerr << "Unable to fork" << std::endl;
-	}
+	cout << mystr;
+
+	std::list<string> mylist;
+
+	mylist.push_back("hello");
+	mylist.push_back("world");
+	mylist.push_back("!");
+
+	std::list<int>::const_iterator list_begin(list.begin());
+
 
 	return 0;
 }

bc5ffb63ef63b20b033f7b57991cefed85ac0553
Julien Amoros <ju.amoros@laposte.net>
2017-09-26 12:21:53 +0200

    test command.hpp + system() pid problem (to solve)

--- test_list.cpp
+++ test_list.cpp
@@ -2,24 +2,24 @@
 #include<unistd.h>
 #include<stdio.h>
 #include<errno.h>
-#include<signal.h>
+#include <signal.h>
 #include <list>
+#include <iostream>
 
 using namespace std;
 
 int main( int argc, char* argv[] ){
-	string mystr = "hello\n";
-
-	cout << mystr;
-
 	std::list<string> mylist;
 
 	mylist.push_back("hello");
 	mylist.push_back("world");
 	mylist.push_back("!");
 
-	std::list<int>::const_iterator list_begin(list.begin());
-
+	std::list<string>::const_iterator
+        	lit (mylist.begin()),
+        	lend(mylist.end());
+    	for(;lit!=lend;++lit) std::cout << *lit << '\n';
+    	std::cout << std::endl;
 
 	return 0;
 }

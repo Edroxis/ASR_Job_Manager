7789fd0101191b4d1cfa94e6f1e92ebc7feb2e8a
Julien Amoros <ju.amoros@laposte.net>
2017-11-21 21:43:17 +0100

    test socket

---
+++ socket/UNIX/client.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int main(int argc, char *argv[])
+{
+	
+}

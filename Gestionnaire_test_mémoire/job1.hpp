6b31531ff82c22009e9b2ba98b1d3e717489a083
myriamllb <myriam.le-liboux@etu.univ-tours.fr>
2017-12-04 15:45:24 +0100

    Test memoire ok

--- Gestionnaire_test_mémoire/job1.hpp
+++ Gestionnaire_test_mémoire/job1.hpp
@@ -8,7 +8,7 @@ class Job1 : public Job{
 	}
 
 	int run(){
-		for (int i=0; i<10000; i++){
+		for (int i=0; i<100000000000000000; i++){
 			double* tab= new double[1000];
 			for (int j=0; j<1000; j++){
 				tab[j]=j;
